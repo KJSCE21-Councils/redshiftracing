0aaabc1251bd73b42016a0609224f1db9c0430b6
baijudodhia <51258945+baijudodhia@users.noreply.github.com>
2020-05-03 02:09:42 +0530

    Performance Improvements

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,6 @@
 document.getElementById("footer").innerHTML = '<footer class="py-2 border-secondary">' +
     '<div class="wrapper">' +
+    '<div id="go-top"><i class="fa fa-arrow-up" onclick="topFunction()"></i></div>' +
     '<div class="py-2">' +
     '<div class="justify-content-center text-center">' +
     '<p class="my-0">Follow Us</p>' +
@@ -22,8 +23,25 @@ document.getElementById("footer").innerHTML = '<footer class="py-2 border-second
     '</div>' +
     '<div class="justify-content-center text-center align-items-center py-2">' +
     '<img src="media/images/rsr-logo.jpg" style="max-height:50px;"/>' +
-    '</div>' +    
+    '</div>' +
     '<div class="footer-copyright text-center py-1">&copy; Copyright: Redshift Racing India 2019-20</a>' +
     '</div>' +
     '</div>' +
     '</footer>';
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         document.getElementById("go-top").style.display = "block";
+//     } else {
+//         document.getElementById("go-top").style.display = "none";
+//     }
+// }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
